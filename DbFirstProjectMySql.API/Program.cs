b91d4b713dae3a55f5bcafc886f75a4d978ce1f7
Pham-Dang-Khoi <phamdangkhoir@gmail.com>
2025-06-13 12:56:33 +0700

    update Connection Strings

--- DbFirstProjectMySql.API/Program.cs
+++ DbFirstProjectMySql.API/Program.cs
@@ -13,8 +13,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Đăng ký DbContext
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(
-        builder.Configuration.GetConnectionString("DefaultConnection"),
-        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
+        builder.Configuration.GetConnectionString("ConnectionStrings__DefaultConnection"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("ConnectionStrings__DefaultConnection"))
     ));
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddAutoMapper(typeof(MappingProfile));
